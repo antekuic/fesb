873a863a8545d0021fe429a3c8635413198960b8
Ante Kuić <73068387+antekuic@users.noreply.github.com>
2020-11-11 15:38:09 +0100

    Update zad5.c

--- Zad5/zad5.c
+++ Zad5/zad5.c
@@ -1,13 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* ZADATAK 5
-Presjek i unija*/
+Presjek i unija
+
+MAINHEAD -> RED -> RED -> RED
+			|		|	   |
+		Elementi Elementi Elementi
+
+LookUP Tablica: Adrese head-a od elementa.
+*/
 
 #define BUFFER_SIZE 256
+#define MAX_LIST 10
 
 typedef struct _Element* ListaP;
+typedef struct _head* HeadP;
+
+typedef struct _head {
+
+	HeadP next_array;
+	ListaP elements;
+
+} Head;
 
 typedef struct _Element {
 
@@ -16,92 +32,113 @@ typedef struct _Element {
 
 } Lista;
 
-int loadData(ListaP, ListaP);
+int loadData(HeadP, ListaP[]);
 int bufferFormating(ListaP, char *);
 int sortUnos(ListaP, int);
 int Ispis(ListaP);
 int DeleteList(ListaP);
+int DeleteAll(HeadP);
 ListaP Unija(ListaP, ListaP); // VRACA ADRESU UNIJE
 ListaP Presjek(ListaP, ListaP); // VRACA ADRESU PRESJEK
 
 int main() {
-	ListaP l1 = (ListaP)malloc(sizeof(Lista)); // HEAD; ( PROVJERE ZA ISPRAVNO MALLOCIRANJE U loadData )
-	l1->next = NULL;
-	ListaP l2 = (ListaP)malloc(sizeof(Lista)); // HEAD;
-	l2->next = NULL;
+	HeadP main_head = (HeadP)(malloc(sizeof(Head)));
 	ListaP rez = NULL; // REZULTAT OPERACIJE
-	int opcija = 0;
+	ListaP lookup[MAX_LIST] = { NULL }; // LOOKUP TABLICA
+	main_head->next_array = NULL;
+	main_head->elements = NULL;
+	
+	int opcija = 0, first = 0, second = 0;
 	
 	// UCITAVANJE LISTI IZ DATOTEKE;
-	if (!loadData(l1, l2)) {
+	if (!loadData(main_head, lookup)) {
 		printf("Ucitane liste: \n");
-		printf("L1: ");
-		Ispis(l1);
-		printf("\nL2: ");
-		Ispis(l2);
-		printf("\n0: UNIJA | 1: PRESJEK:\nUnesite opciju: ");
+		for (int i = 0; i < MAX_LIST; i++)
+		{
+			if (lookup[i] != NULL)
+			{
+				printf("L%d: ", i + 1);
+				Ispis(lookup[i]);
+				printf("\n");
+			}
+			else break;
+		}
+		
+		printf("\n0: UNIJA | 1: PRESJEK | 3: GASENJE PROGRAMA:\nUnesite opciju(opcija id_prviclan id_drugiclan): ");
 
 		while (opcija != -1)
 		{
-			scanf(" %d", &opcija);
-			switch (opcija) {
-			case 0:
-				if (rez != NULL)
-				{
-					DeleteList(rez);
-				}
-				rez = Unija(l1, l2);
-				printf("L1 U L2: ");
-				Ispis(rez);
-				printf("\n");
-				break;
-			case 1:
-				if (rez != NULL)
-				{
-					DeleteList(rez);
-				}
-				rez = Presjek(l1, l2);
-				printf("L1 PRESJEK L2: ");
-				Ispis(rez);
-				printf("\n");
-				break;
-			default:
-				opcija = -1;
-				printf("\nProgram gotov. Sva memorija ociscena.");
-				break;
+			scanf(" %d %d %d", &opcija, &first, &second);
+			if ((opcija == 0 || opcija == 1) && ((first <= 0 || second <= 0) || lookup[first-1] == NULL || lookup[second-1] == NULL)) {
+				printf("\nUnijeli ste neispravne clanove. Ponovite.");
 			}
-			printf("\nUnesite opciju: ");
+			else {
+				switch (opcija) {
+				case 0:
+					if (rez != NULL)
+					{
+						DeleteList(rez);
+					}
+					rez = Unija(lookup[first - 1], lookup[second - 1]);
+					printf("L%d U L%d: ", first, second);
+					Ispis(rez);
+					printf("\n");
+					break;
+				case 1:
+					if (rez != NULL)
+					{
+						DeleteList(rez);
+					}
+					rez = Presjek(lookup[first - 1], lookup[second - 1]);
+					printf("L%d PRESJEK L%d: ", first, second);
+					Ispis(rez);
+					printf("\n");
+					break;
+				default:
+					opcija = -1;
+					printf("\nProgram gotov. Sva memorija ociscena.");
+					break;
+				}
+			}
+			if (opcija != -1) {
+				printf("\nUnesite opciju: ");
+			}
 		}
 	}
 
-	DeleteList(rez);
-	free(rez);
-	DeleteList(l1);
-	free(l1);
-	DeleteList(l2);
-	free(l2);
-
+	if (rez != NULL) {
+		DeleteList(rez);
+		free(rez);
+	}
+	DeleteAll(main_head);
+	free(main_head);
 	system("pause");
 	return 0;
 }
 
-int loadData(ListaP l1, ListaP l2) {
-	if (l1 != NULL && l2 != NULL) {
+int loadData(HeadP main_head, ListaP lookup[]) {
+	if (main_head != NULL) {
 		FILE *f = NULL;
+		HeadP pom = NULL;
 		char string[BUFFER_SIZE];
 		int d = 0; // Za odredivanje liste.
 		f = fopen("niz.txt", "r");
 		if (f == NULL) return -1;
 		while (1) {
-			if (d == 2) break;
+			if (d == MAX_LIST) break;
 			if (fgets(string, BUFFER_SIZE, f) != NULL) {
-				if (d == 0) {
-					bufferFormating(l1, string);
+				if (strlen(string) > 1)
+				{
+					pom = main_head->next_array;
+					main_head->next_array = (HeadP)malloc(sizeof(Head));
+					main_head->next_array->next_array = pom;
+					main_head->next_array->elements = (ListaP)malloc(sizeof(Lista));
+					lookup[d] = main_head->next_array->elements;
+					main_head->next_array->elements->next = NULL;
+					bufferFormating(main_head->next_array->elements, string);
+					d++;
 				}
-				else {
-					bufferFormating(l2, string);
-				}
-				d++;
+				if (feof(f)) break;
 			}
 		}
 		return 0;
@@ -241,6 +278,28 @@ int DeleteList(ListaP l)
 		if (l != NULL)
 		{
 			free(l);
+		}
+		return 0;
+	}
+	return -1;
+}
+
+int DeleteAll(HeadP head)
+{
+	if (head != NULL)
+	{
+		head = head->next_array;
+		HeadP pom = NULL;
+		while (head != NULL && head->next_array != NULL)
+		{
+			pom = head->next_array;
+			head->next_array = head->next_array->next_array;
+			DeleteList(pom->elements);
+			free(pom);
+		}
+		if (head != NULL)
+		{
+			free(head);
 		}
 		return 0;
 	}
